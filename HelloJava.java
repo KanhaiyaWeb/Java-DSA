9031c46dd1b93977af661e76280a66fee954d4b8
KanhaiyaWeb <133254282+KanhaiyaWeb@users.noreply.github.com>
2023-12-23 22:09:46 +0530

    Day1 - Handshake with Java

---
+++ HelloJava.java
@@ -0,0 +1,12 @@
+class HellowJava{
+    public static void main(String[] args) {
+        System.out.println("Hello  Java ! How are you finally i m here to learn you how you work");
+    }
+}
+
+// I Java everthig is written in class and class is the main container
+// IF  there are more then 1 file we add pulic classs 
+// In java file name should be Camel Case 
+// classs name should be same as file name 
+// class name should start with capital letter
+// System.out.println - is used to print in console
