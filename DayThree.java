fdb90412fda74c3ad5a60c42c61e44b35474be74
Kanhaiya Kumar <133254282+KanhaiyaWeb@users.noreply.github.com>
2023-12-29 12:32:02 +0530

    Update DayThree.java
    Notes updates

--- DayThree.java
+++ DayThree.java
@@ -74,6 +74,23 @@ class PrimitiveClass{
         // Primitive data Type means which you can not break further into another data type is called primitive data type 
 
 
+        // boolean check = true;              1 bit  Stores true or false values
+
+        // byte a = 1;          1 byte  Stores whole numbers from -128 to 127
+
+        // short b = 1;        2 bytes  Stores whole numbers from -32,768 to 32,767
+
+        // char letter = 's';           2 bytes  Stores a single character/letter or ASCII values
+
+        // int rollno = 64;             4 bytes  Stores whole numbers from -2,147,483,648 to 2,147,483,647
+
+        // float marks = 98.67f;        4 bytes  Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+
+        // double largeDecimalNumbers = 4534534534.454353;      8 bytes  Stores fractional numbers. Sufficient for storing 15 decimal digits
+
+        // long largeIntergerValues = 4245345346356354345L;     8 bytes  Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+
         //  char starts which the single quote and syring starts with the double quote 
 
         // eg : 
@@ -107,6 +124,9 @@ public static void main(String[] args) {
     System.out.println(  a+b);
 }
 }
+
+
+
 
 
 
